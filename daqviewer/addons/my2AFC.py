9a7bda94b14509a78c072a0737525547a92bfbe3
Sumiya Kuroda <26479377+sumiya-kuroda@users.noreply.github.com>
2024-09-06 02:13:23 +0100

    Add example config

---
+++ daqviewer/addons/my2AFC.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QWidget
+
+class DAQViewerAddOn(QWidget):
+    def __init__(self, parent=None, config=None, **kwargs):
+        super().__init__(parent=parent, **kwargs)
+        pass
